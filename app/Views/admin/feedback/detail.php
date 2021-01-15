4774d5f744e6f8bf9584b9fe64c001e0491294fe
irfakwahyudi <irfakwahyudi2@gmail.com>
2021-01-15 22:03:15 +0700

    mengubah posisi

--- app/Views/admin/feedback/detail.php
+++ app/Views/admin/feedback/detail.php
@@ -1,105 +1,119 @@
  <!-- Content Wrapper. Contains page content -->
- <div class="content-wrapper"> 
-    <!-- Content Header (Page header) -->
-    <div class="content-header sty-one">
-      <h1>Detail Feedback </h1>
-      <ol class="breadcrumb">
-        <li><a href="/superAdmin/reportFeedback">Laporan Feedback</a></li>
-        <li><i class="fa fa-angle-right"></i> Detail Feedback</li>
-      </ol>
-    </div>
-    
-    <!-- Main content -->
-    <div class="content"> 
-      <!-- Small boxes (Stat box) -->
-      <div class="page-content">
-        <!-- Page Header-->
-        <!-- Page Content -->
-        <div class="col-lg-12">
-                <div class="block">
-                  <div class="block-body">
-                    <form class="form-horizontal">
-                      <div class="form-group row">
-                        <label class="col-sm-1 form-control-label"><strong>Nama</strong></label>
-                        <div class="col-sm-3">
-                          <input type="text" class="form-control">Nama</label>
-                        </div>
-                        <label class="col-sm-1 form-control-label">Alamat</label>
-                        <div class="col-sm-4">
-                          <input type="text" class="form-control">
-                        </div>
-                        <label class="col-sm-1 form-control-label">No.HP</label>
-                        <div class="col-sm-2">
-                          <input type="text" class="form-control">
-                        </div>
-                        </div>
-                        <div class="line"></div>
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Penyebab</strong></label>
-                            <div class="col-sm-11">
-                            <input type="textarea" class="form-control"><small class="help-block-none">Penyebab Kejadian, Tuliskan secara lengkap dan detail.</small>
-                            </div>
-                        </div>          
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Q1</strong></label>
-                            <div class="col-sm-11">
-                                <div class="form-group">
-                                    <label class="form-control-label"><strong>Apa Benar Bapak/Ibu pernah Menghubungi Ngalam 112 ?</strong></label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                            </div>
-                        </div>          
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Q2</strong></label>
-                            <div class="col-sm-11">
-                                <div class="form-group">
-                                    <label class="form-control-label"><strong>Apakah Bapak merasa terbantu dengan adanya layanan Ngalam 112 ?</strong></label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                            </div>
-                        </div>          
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Q3</strong></label>
-                            <div class="col-sm-11">
-                                <div class="form-group">
-                                    <label class="form-control-label"><strong>Menurut Bapak/Ibu, Apakah Pelayanan Ngalam 112 dibutuhkan atau Tidak ?</strong></label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                            </div>
-                        </div>          
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Q4</strong></label>
-                            <div class="col-sm-11">
-                                <div class="form-group">
-                                    <label class="form-control-label"><strong>Bagaimana Pendapat Bapak/Ibu tentang Pelayanan Ngalam 112 ? Berikan Penilaian !</strong></label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                            </div>
-                        </div>          
-                        <div class="form-group row">
-                            <label class="col-sm-1 form-control-label"><strong>Q5</strong></label>
-                            <div class="col-sm-11">
-                                <div class="form-group">
-                                    <label class="form-control-label"><strong>Apakah ada saran untuk layanan 112 ?</strong></label>
-                                    <input type="textarea" placeholder="" class="form-control">
-                                </div>
-                            </div>
-                        </div>          
-                        <div class="line"></div>
-                        <div class="form-group row">
-                            <div class="col-sm-11 ml-auto">
-                          <button type="submit" class="btn btn-primary">Back</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-</div>
-    </div>
-    <!-- /.content --> 
-  </div>
- 
+ <div class="content-wrapper">
+   <!-- Content Header (Page header) -->
+   <div class="content-header sty-one">
+     <h1>Detail Feedback </h1>
+     <ol class="breadcrumb">
+       <li><a href="/superAdmin/reportFeedback">Laporan Feedback</a></li>
+       <li><i class="fa fa-angle-right"></i> Detail Feedback</li>
+     </ol>
+   </div>
 
+   <!-- Main content -->
+   <div class="content">
+     <!-- Small boxes (Stat box) -->
+     <div class="page-content">
+       <!-- Page Header-->
+       <!-- Page Content -->
+       <div class="col-lg-12">
+         <div class="block">
+           <div class="block-body">
+             <form class="form-horizontal">
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Nama</strong></label>
+                 <div class="col-sm-5">
+                   <input type="text" class="form-control">
+                 </div>
+                 <label class="col-sm-1 form-control-label"><strong>No.HP</strong></label>
+                 <div class="col-sm-3">
+                   <input type="text" class="form-control">
+                 </div>
+               </div>
+               <div class="line"></div>
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Alamat</strong></label>
+                 <div class="col-sm-5">
+                   <input type="text" class="form-control">
+                 </div>
+               </div>
+               <div class="line"></div>
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Penyebab</strong></label>
+                 <div class="col-sm-5">
+                   <input type="textarea" class="form-control">
+                 </div>
+               </div>
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Q1</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Apa Benar Bapak/Ibu pernah Menghubungi Ngalam 112 ?</strong></label>
+                     <input type="text" placeholder="" class="form-control">
+                   </div>
+                 </div>
+                 <label class="col-sm-1 form-control-label"><strong>Q4</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Bagaimana Pendapat Bapak/Ibu tentang Pelayanan Ngalam 112 ? Berikan Penilaian !</strong></label>
+                     <input type="text" placeholder="" class="form-control">
+                   </div>
+                 </div>
+               </div>
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Q2</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Apakah Bapak merasa terbantu dengan adanya layanan Ngalam 112 ?</strong></label>
+                     <input type="text" placeholder="" class="form-control">
+                   </div>
+                 </div>
+                 <label class="col-sm-1 form-control-label"><strong>Q5</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Apakah ada saran untuk layanan 112 ?</strong></label>
+                     <input type="textarea" placeholder="" class="form-control">
+                   </div>
+                 </div>
+               </div>
+               <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Q3</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Menurut Bapak/Ibu, Apakah Pelayanan Ngalam 112 dibutuhkan atau Tidak ?</strong></label>
+                     <input type="text" placeholder="" class="form-control">
+                   </div>
+                 </div>
 
-        
+               </div>
+               <!-- <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Q4</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Bagaimana Pendapat Bapak/Ibu tentang Pelayanan Ngalam 112 ? Berikan Penilaian !</strong></label>
+                     <input type="text" placeholder="" class="form-control">
+                   </div>
+                 </div>
+               </div> -->
+               <!-- <div class="form-group row">
+                 <label class="col-sm-1 form-control-label"><strong>Q5</strong></label>
+                 <div class="col-sm-4">
+                   <div class="form-group">
+                     <label class="form-control-label"><strong>Apakah ada saran untuk layanan 112 ?</strong></label>
+                     <input type="textarea" placeholder="" class="form-control">
+                   </div>
+                 </div>
+               </div> -->
+               <div class="line"></div>
+               <div class="form-group row">
+                 <div class="col-sm-11 ml-auto">
+                   <button type="submit" class="btn btn-primary">Kembali</button>
+                 </div>
+               </div>
+             </form>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+   <!-- /.content -->
+ </div>
--- app/Views/admin/reportfeedback.php
+++ app/Views/admin/reportfeedback.php
@@ -1,116 +1,114 @@
  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper"> 
-    <!-- Content Header (Page header) -->
-    <div class="content-header sty-one">
-      <h1>Laporan Feedback </h1>
-      <ol class="breadcrumb">
-        <li><a href="#">Home</a></li>
-        <li><i class="fa fa-angle-right"></i> Laporan Feedback</li>
-      </ol>
-    </div>
-    
-    <!-- Main content -->
-    <div class="content"> 
-      <!-- Small boxes (Stat box) -->
-      <div class="page-content">
-        <!-- Page Header-->
-        <section class="no-padding-top">
-          <div class="container-fluid">
-              <div class="row">
-              <div class="col-lg-12">
-                <div class="block">
-                  <div class="title"><strong>Tabel Feedback</strong></div>
-                  <div class="table-responsive"> 
-                    <table class="table table-striped table-hover" id="tables"> 
-                      <thead>
-                        <tr>
-                          <th>ID</th>
-                          <th>Nama</th>
-                          <th>Alamat</th>
-                          <th>No. HP</th>
-                          <!-- <th>Tanggal</th> -->
-                          <th>Penyebab</th>
-                          <th>Q1</th>
-                          <th>Q2</th>
-                          <th>Q3</th>
-                          <th>Q4</th>
-                          <th>Q5</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php foreach ($feedback as $r ) :?>
-                        <tr>
-                          <th scope="row"><?= $r['id_feedback'];?></th>
-                          <td><?= $r['nama_feedback'];?></td>
-                          <td><?= $r['alamat_feedback'];?></td>
-                          <td><?= $r['noHp_feedback'];?></td>
-                          <td><?= $r['penyebab_feedback'];?></td>
-                          <td><?= $r['q1_feedback'];?></td>
-                          <td><?= $r['q2_feedback'];?></td>
-                          <td><?= $r['q3_feedback'];?></td>
-                          <td><?= $r['q4_feedback'];?></td>
-                          <td><?= $r['q5_feedback'];?></td>
-                          <td>
-                                <a href="/feedback/detailFeedback/<?= $r['id_feedback']; ?>" class="btn btn-info btn-sm">Detail</a>
-                                <a href="/feedback/delete/<?= $r['id_feedback']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin data ini akan dihapus');">Delete</a>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>                         
-                      </tbody>
-                    </table>
-                  </div>
-                  <div>
-                    <!-- <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary center">Tambah Feedback</button> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            </div>
-            </div>
-          </div>
-           <div id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" class="modal fade text-left" aria-hidden="true" style="display: none;">
-                      <div role="document" class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header"><strong id="exampleModalLabel" class="modal-title">Tambah Feedback</strong>
-                            <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
-                          </div>
-                          <div class="modal-body">
-                            <form>
-                              <div class="form-group">
-                                <label>Nama</label>
-                                <input type="nama" placeholder="Email Address" class="form-control">
-                              </div>
-                              <div class="form-group">       
-                                <label>Alamat</label>
-                                <input type="password" placeholder="Password" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                <label>No.HP</label>
-                                <input type="email" placeholder="Email Address" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                <label>Tanggal Kejadian</label>
-                                <input type="email" placeholder="Email Address" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                <label>Penyebab</label>
-                                <input type="email" placeholder="Email Address" class="form-control">
-                              </div>
-                            </form>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" data-dismiss="modal" class="btn btn-secondary">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-        </section> 
-    </section>
-    </div>        
-    <!-- /.content --> 
-  </div>
+ <div class="content-wrapper">
+   <!-- Content Header (Page header) -->
+   <div class="content-header sty-one">
+     <h1>Laporan Feedback </h1>
+     <ol class="breadcrumb">
+       <li><a href="#">Home</a></li>
+       <li><i class="fa fa-angle-right"></i> Laporan Feedback</li>
+     </ol>
+   </div>
 
-
-        
+   <!-- Main content -->
+   <div class="content">
+     <!-- Small boxes (Stat box) -->
+     <div class="page-content">
+       <!-- Page Header-->
+       <section class="no-padding-top">
+         <div class="container-fluid">
+           <div class="row">
+             <div class="col-lg-12">
+               <div class="block">
+                 <div class="title"><strong>Tabel Feedback</strong></div>
+                 <div class="table-responsive">
+                   <table class="table table-striped table-hover" id="tables">
+                     <thead>
+                       <tr>
+                         <th>ID</th>
+                         <th>Nama</th>
+                         <th>Alamat</th>
+                         <th>No. HP</th>
+                         <!-- <th>Tanggal</th> -->
+                         <th>Penyebab</th>
+                         <th>Q1</th>
+                         <th>Q2</th>
+                         <th>Q3</th>
+                         <th>Q4</th>
+                         <th>Q5</th>
+                         <th>Action</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       <?php foreach ($feedback as $r) : ?>
+                         <tr>
+                           <th scope="row"><?= $r['id_feedback']; ?></th>
+                           <td><?= $r['nama_feedback']; ?></td>
+                           <td><?= $r['alamat_feedback']; ?></td>
+                           <td><?= $r['noHp_feedback']; ?></td>
+                           <td><?= $r['penyebab_feedback']; ?></td>
+                           <td><?= $r['q1_feedback']; ?></td>
+                           <td><?= $r['q2_feedback']; ?></td>
+                           <td><?= $r['q3_feedback']; ?></td>
+                           <td><?= $r['q4_feedback']; ?></td>
+                           <td><?= $r['q5_feedback']; ?></td>
+                           <td>
+                             <a href="/feedback/detailFeedback/<?= $r['id_feedback']; ?>" class="btn btn-info btn-sm">Detail</a>
+                             <a href="/feedback/delete/<?= $r['id_feedback']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin data ini akan dihapus');">Delete</a>
+                           </td>
+                         </tr>
+                       <?php endforeach; ?>
+                     </tbody>
+                   </table>
+                 </div>
+                 <div>
+                   <!-- <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary center">Tambah Feedback</button> -->
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+     </div>
+   </div>
+   <div id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" class="modal fade text-left" aria-hidden="true" style="display: none;">
+     <div role="document" class="modal-dialog">
+       <div class="modal-content">
+         <div class="modal-header"><strong id="exampleModalLabel" class="modal-title">Tambah Feedback</strong>
+           <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
+         </div>
+         <div class="modal-body">
+           <form>
+             <div class="form-group">
+               <label>Nama</label>
+               <input type="nama" placeholder="Email Address" class="form-control">
+             </div>
+             <div class="form-group">
+               <label>Alamat</label>
+               <input type="password" placeholder="Password" class="form-control">
+             </div>
+             <div class="form-group">
+               <label>No.HP</label>
+               <input type="email" placeholder="Email Address" class="form-control">
+             </div>
+             <div class="form-group">
+               <label>Tanggal Kejadian</label>
+               <input type="email" placeholder="Email Address" class="form-control">
+             </div>
+             <div class="form-group">
+               <label>Penyebab</label>
+               <input type="email" placeholder="Email Address" class="form-control">
+             </div>
+           </form>
+         </div>
+         <div class="modal-footer">
+           <!-- <button type="button" data-dismiss="modal" class="btn btn-secondary">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button> -->
+           <button type="submit" class="btn btn-success">Simpan</button>
+         </div>
+       </div>
+     </div>
+   </div>
+   </section>
+   </section>
+ </div>
+ <!-- /.content -->
+ </div>
