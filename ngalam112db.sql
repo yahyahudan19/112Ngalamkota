5684c2ad584b5d96c0616589247eff1bbf572240
irfakwahyudi <irfakwahyudi2@gmail.com>
2021-01-20 12:16:55 +0700

    database baruuuuu

--- ngalam112db.sql
+++ ngalam112db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2021 at 08:15 AM
+-- Generation Time: Jan 20, 2021 at 06:16 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -106,7 +106,10 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id_news`, `tagline_news`, `judul_news`, `isi_news`, `link_news`, `date_news`, `dokumentasi_news`, `created_at`, `updated_at`) VALUES
-(14, 'ini tagline', 'ini judulnya yaa', ' s cdsb cjkjkdc dv dv', 'https://github.com/yahyahudan19/112Ngalamkota', '2021-01-14', '1610952806_d7fbc0f8b0758843db6e.jpeg', '2021-01-18', '2021-01-18');
+(22, 'ini tagline', 'ini judulnya yaa', 'kyuytf', 'https://github.com/yahyahudan19/112Ngalamkota', '2021-01-13', '1611033163_53199d2c9e164752cf33.jpg', '2021-01-18', '2021-01-18'),
+(23, 'halo', 'ini judulnya yaa', 'jbhvh', 'https://www.liputan6.com/tag/kebakaran', '2021-01-14', '1611033228_e76c00647bb7807635a8.jpg', '2021-01-18', '2021-01-18'),
+(24, 'ini tagline', 'hahahakdnkdnw', 'mdkfmk', 'https://www.liputan6.com/tag/kebakaran', '2021-01-06', '1611033800_f971aef1599eb0f926eb.jpeg', '2021-01-18', '2021-01-18'),
+(25, 'Kebakaran', 'KMLSKAMKLASD', 'Pengenalan: Bagian ini akan memberikan penjelasan yang singkat mengenai peristiwa sejarah yang akan diceritakan.\r\nUrutan Keja', 'A;LSDM;LMDLADM', '2021-01-20', '1611040296_11855e8ab3a9f87c7d43.png', '2021-01-19', '2021-01-19');
 
 -- --------------------------------------------------------
 
@@ -129,10 +132,8 @@ CREATE TABLE `pengumuman` (
 --
 
 INSERT INTO `pengumuman` (`id_pengumuman`, `tagline_pengumuman`, `judul_pengumuman`, `isi_pengumuman`, `date_pengumuman`, `created_at`, `updated_at`) VALUES
-(2, 'kebakaran', 'irfak pandai', 'Dugaan sementara api berasal dari arus pendek yang menyebabkan percikan api, dan merambat ke material yang mudah terbakar, se', '2021-01-10', '2021-01-16', '2021-01-16'),
-(4, 'dwdw', 'Jam Kerja Layanan 112', 'wdefm               ', '2021-01-06', '2021-01-17', '2021-01-17'),
-(5, 'Jam Kerja', 'Layanana akan bekerja pada jam kerja', 'SIAP SEDIA', '2021-01-18', '2021-01-17', '2021-01-17'),
-(6, 'wddwdw', 'Jam Kerja Layanan 112', 'ntab', '2021-01-18', '2021-01-17', '2021-01-17');
+(4, 'dwdw', 'Jam Kerja Layanan 112 a', 'wdefm   hahaha            ', '2021-01-06', '2021-01-17', '2021-01-17'),
+(5, 'Jam Kerja', 'Layanana akan bekerja pada jam kerja', 'SIAP SEDIA', '2021-01-18', '2021-01-17', '2021-01-17');
 
 -- --------------------------------------------------------
 
@@ -154,7 +155,7 @@ CREATE TABLE `report` (
 --
 
 INSERT INTO `report` (`id_pelapor`, `kejadian`, `lokasi_kejadian`, `tanggal`, `nama_pelapor`, `tindak_lanjut`) VALUES
-(51, 'Pohon Tumbang', 'Jakarta', '2021-01-12', 'Irfak', 'Sudah ditangani Pemadam'),
+(51, 'Pohon Tumbang Hara', 'Jakarta', '2021-01-12', 'Irfak', 'Sudah ditangani Pemadam'),
 (52, 'Kebakaran', 'Jakarta', '2021-01-19', 'Irfak Wahyudi', 'SUDAH'),
 (53, 'Kebakaran', 'Malang', '2021-01-15', 'Irfak', 'Sudah ditangani Pemadam'),
 (54, 'Pohon Tumbang', 'Malang', '2021-01-15', 'Irfak Wahyudi', 'Sudah ditangani Pemadam'),
@@ -171,37 +172,20 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `image` text NOT NULL,
-  `role_id` int(1) NOT NULL,
-  `is_active` int(1) NOT NULL
+  `level` varchar(20) NOT NULL,
+  `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `email`, `username`, `password`, `image`, `role_id`, `is_active`) VALUES
-(1, 'diskominfo_ngalamkota@gmail.go.id', 'superadmin', '123456', 'default.jpg', 1, 1),
-(3, 'ngalam112@malangkota.go.id', 'admin', '123456', '', 2, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_role`
---
-
-CREATE TABLE `user_role` (
-  `id` int(3) NOT NULL,
-  `role` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_role`
---
-
-INSERT INTO `user_role` (`id`, `role`) VALUES
-(1, 'SuperAdmin'),
-(2, 'Admin');
+INSERT INTO `user` (`id_user`, `email`, `username`, `password`, `level`, `image`) VALUES
+(1, 'diskominfo_ngalamkota@gmail.go.id', 'superadmin', '123456', 'Super Admin', 'default.jpg'),
+(3, 'ngalam112@malangkota.go.id', 'admin', '123456', 'Admin', ''),
+(4, 'irfakwahyudi3@gmail.com', 'irfak', 'irfak', 'Super Admin', ''),
+(5, 'irfakwahyudi2@gmail.com', 'master irfak', '123456789', 'Admin', ''),
+(6, 'haha@gmail.com', 'Irfak Wahyudi', 'master', 'Super Admin', '');
 
 --
 -- Indexes for dumped tables
@@ -244,12 +228,6 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `user_role`
---
-ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -269,13 +247,13 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id_news` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_news` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `pengumuman`
 --
 ALTER TABLE `pengumuman`
-  MODIFY `id_pengumuman` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_pengumuman` int(125) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `report`
@@ -287,13 +265,7 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user_role`
---
-ALTER TABLE `user_role`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
